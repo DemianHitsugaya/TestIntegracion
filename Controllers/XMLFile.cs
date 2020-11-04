3399df543ee80771709b15e2e20aa68ae531b8f5
Ricardo Ortega <togi_ortega@hotmail.com>
2020-11-04 12:03:11 -0500

    Initial commit

---
+++ Controllers/XMLFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestIntegracion.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class XMLFile : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetText()
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("Technical Test - File 1.txt");
+                var path = "methodResponse/params/param";
+
+                var stringText = string.Empty;
+                foreach (XmlNode item in xmlDoc.SelectNodes(path))
+                {
+                    if (string.IsNullOrEmpty(item.InnerText))
+                        throw new Exception("The item String not contain Text");
+                    else
+                    {
+                       stringText = item.InnerText;
+                    }
+                }
+
+                return Ok(stringText);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
